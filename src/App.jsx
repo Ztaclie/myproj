de6db837f006fd3ed422edd50858308a38d3333f
Ztaclie <melmeh42@gmail.com>
2025-09-02 22:51:10 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1 className="text-amber-700 text-5xl">Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
